deff11f78023e9c9512931bc7c97e9b6876617a4
abrahon <mdabrahon926@gmail.com>
2022-10-26 03:00:52 +0600

    added categories data

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 const express = require('express')
 const app = express();
+const cors =require('cors');
 const port = process.env.PORT || 5000;
+
+
+app.use(cors());
+const categories = require('./data/categories.json');
+
 
 app.get('/',(req,res)=>{
     res.send('Course API Running');
 });
+
+app.get('/courses-categories',(req,res)=>{
+   // console.log('online-courses-server',)
+   res.send( categories)
+})
 app.listen(port,()=>{
     console.log('online courses server',port);
 })
