fba6a6265296fca6358dfb87dc385a924d9333db
abrahon <mdabrahon926@gmail.com>
2022-10-26 21:30:38 +0600

    added courses id

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 const categories = require('./data/categories.json');
+const courses = require('./data.courses.json');
 
 
 app.get('/',(req,res)=>{
@@ -15,6 +16,16 @@ app.get('/',(req,res)=>{
 app.get('/courses-categories',(req,res)=>{
    // console.log('online-courses-server',)
    res.send( categories)
+});
+app.get('/category/:id',(req, res)=>{
+    const id=req.params.id;
+    const categoryCourses = courses.filter(courses=>courses.id===id);
+    res.send(categoryCourses);
+})
+app.get('/courses/:id',(req,res)=>{
+    const id = req.params.id;
+    const selectedCourses= courses.find(course=>course.id===id);
+    res.send(selectedCourses);
 })
 app.listen(port,()=>{
     console.log('online courses server',port);
